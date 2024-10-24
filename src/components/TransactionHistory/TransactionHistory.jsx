b77977b5725aa7d947b2591765e288ca66149cfb
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-10-24 23:33:07 +0300

    task 3

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,8 +1,26 @@
-// import transStyles from "./TransactionHistory.module.css"
+import transStyles from "./TransactionHistory.module.css"
 
-const TransactionHistory = () => {
+const TransactionHistory = ({items}) => {
   return (
-    <div>TransactionHistory</div>
+  <table className={transStyles.table}>
+    <thead className={transStyles.title}>
+      <tr>
+        <th className={transStyles.titleCells}>Type</th>
+        <th className={transStyles.titleCells}>Amount</th>
+        <th className={transStyles.titleCells}>Currency</th>
+      </tr>
+    </thead>
+  
+    <tbody>
+      {items.map((item) => {
+        return <tr key={item.id} className={transStyles.transItem}>
+            <td className={transStyles.rowCells}>{item.type}</td>
+            <td className={transStyles.rowCells}>{item.amount}</td>
+            <td className={transStyles.rowCells}>{item.currency}</td>
+        </tr>  
+      })}
+    </tbody>
+</table>
   )
 }
 
